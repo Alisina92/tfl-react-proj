8382b3b4490c700c3e940e64c1174b4ccf02edb1
[Alisina92] <asb92.jobs@gmail.com>
2020-06-16 13:07:48 +0100

    TFL Proj-Alisina level 100 Completed

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Origination from './Origination';
+import Title from './Title';
+
+ /*Heading will have  Title origination and destination*/
+const Heading =()=>{
+  return<div>
+      <Title/>
+      <Origination/>
+      
+      </div>
+}
+
+
+export default Heading
---
+++ src/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+/*Rending Title of the App*/
+const Title = () => {
+    return<h1>
+       Transport For London Line Information 
+    </h1>
+};
+
+export default Title;
